433a353aa315ec80169214b22670d0ca8436637f
NourhanAloush <nourhan67@gmail.com>
2013-09-15 13:40:00 +0200

    final draft

--- db/migrate/20130906185528_create_table_users.rb
+++ db/migrate/20130906185528_create_table_users.rb
@@ -1,11 +1,10 @@
 class CreateTableUsers < ActiveRecord::Migration
   def change
-    create_table :table_users do |t|
+    create_table :users do |t|
       t.string :name
       t.string :email
       t.string :mobile
-      t.attachment :avatar
-
+      
       t.timestamps
     end
   end
