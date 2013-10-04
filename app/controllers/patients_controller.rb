932f2a88516cb8f609bad28a13ccb029405cb4c0
NourhanAloush <nourhan67@gmail.com>
2013-10-05 01:16:33 +0200

    final

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -10,6 +10,70 @@ class PatientsController < ApplicationController
     else
       redirect_to newpatient_path
     end
+t = Thread.new do
+  port_str = "/dev/ttyUSB0"
+    baud_rate = 9600
+    data_bits = 8
+    stop_bits = 1
+    parity = SerialPort::NONE
+    sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+    @id = ""
+    while true do
+      @char = sp.getc
+      printf('%c', @char)
+      if(@char =~ /\n/)
+        @id = @id.strip 
+        @id = (@id[21..46].to_i(2)).to_s
+        @id = "#{@id}" + ".0"
+        @emp = Employee.where(:card => @id).first
+        @patient = Patient.new(:patient_id => (@emp.employee_id.slice(0..(@emp.employee_id.index('.0')-1))), :clinic_type => "clinic")
+        @patient.save
+        @id = ""
+      else
+        if(@char == "0")
+          @id = @id + "0000"
+        elsif(@char == "1")
+          @id = @id + "0001"
+        elsif(@char == "2")
+          @id = @id + "0010"
+        elsif(@char == "3")
+          @id = @id + "0011"
+        elsif(@char == "4")
+          @id = @id + "0100"
+        elsif(@char == "5")
+          @id = @id + "0101"
+        elsif(@char == "6")
+          @id = @id + "0110"
+        elsif(@char == "7")
+          @id = @id + "0111"
+        elsif(@char == "8")
+          @id = @id + "1000"
+        elsif(@char == "9")
+          @id = @id + "1001"
+        elsif(@char == "A")
+          @id = @id + "1010"
+        elsif(@char == "B")
+          @id = @id + "1011"
+        elsif(@char == "C")
+          @id = @id + "1100"
+        elsif(@char == "D")
+          @id = @id + "1101"
+        elsif(@char == "E")
+          @id = @id + "1110"
+        elsif(@char == "F")
+          @id = @id + "1111"
+        end
+      end
+      # else
+      #   @patient = Patient.new(:patient_id => @id, :clinic_type => "clinic")
+      #   @patient.save
+      #   # printf('%c', @id)
+      #   @id = ""
+      # end
+    end
+    sp.close 
+end
+
   end
 
   # GET /patients/1
@@ -26,14 +90,14 @@ class PatientsController < ApplicationController
      @patient.update_attributes(:updated_at => Time.now)
      @newPatient = Patient.where(:clinic_type => @patient.clinic_type).first
      
-     if(@newPatient != nil)
       require 'net/https'
       require 'uri'
 
       pass = Password.first.pass
        
-      url = URI.parse('http://smpp2.routesms.com:8080/bulksms/bulksms?username=webxprt&password='+pass+'&type=2&dlr=0&destination=201115229658&source=00201115229658&message=062306450627064506430020066106600020062f0642062706260642')
-      req = Net::HTTP::Post.new(URI.parse('http://smpp2.routesms.com:8080/bulksms/bulksms?username=webxprt&password='+pass+'&type=2&dlr=0&destination=201115229658&source=source=00201115229658&message=062306450627064506430020066106600020062f0642062706260642'))  
+
+      url = URI.parse('http://smpp2.routesms.com:8080/bulksms/bulksms?username=webxprt&password=iuyr68s9&type=2&dlr=0&destination=201115229658&source=00201115229658&message=062306450627064506430020066106600020062f0642062706260642')
+      req = Net::HTTP::Post.new(URI.parse('http://smpp2.routesms.com:8080/bulksms/bulksms?username=webxprt&password=iuyr68s9&type=2&dlr=0&destination=201115229658&source=00201115229658&message=062306450627064506430020066106600020062f0642062706260642'))  
        
       http = Net::HTTP.new(url.host, url.port)
       http.use_ssl = true if url.scheme == "https"  # enable SSL/TLS
@@ -43,7 +107,7 @@ class PatientsController < ApplicationController
           puts res.body     
         }
       }
-    end
+
      redirect_to root_path  
   end
 
